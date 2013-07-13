43cc30701cbbb283204e6b11e9a18cfb12c6fbde
Sam Wong <sam.wong.1990@gmail.com>
2013-07-13 01:09:50 +0100

    Q11, cleanup

--- 1.py
+++
@@ -1,3 +0,0 @@
-import platform
-
-print(sum(filter(lambda x: x%3 == 0 or x%5 == 0, range(1,1000))))
--- 16.php
+++
@@ -1,7 +0,0 @@
-<?php
-$result = bcpow('2', '1000');
-$sum = 0;
-for($i=0; $i<strlen($result); $i++){
-	$sum += $result[$i];
-}
-echo "Answer = $sum;";
